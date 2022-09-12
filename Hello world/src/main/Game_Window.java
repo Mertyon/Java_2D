e25ece04c4fd2c4425ffe310ff468af8cc44ff7d
Mertyon <przemek_tru@wp.pl>
2022-09-12 10:33:18 +0200

    KeyHandler
    There is a problem with uppdating position of square

--- Hello world/src/main/Game_Window.java
+++ Hello world/src/main/Game_Window.java
@@ -19,14 +19,25 @@ public class Game_Window extends JPanel implements Runnable{
 	final int max_Screen_Width = tileSize * max_Screen_Column;
 	final int max_Screen_Height = tileSize * max_Screen_Row;
 	
+	final int FPS = 60;
+	
+	Keyboard_Handler keyHandler = new Keyboard_Handler();
+
 	Thread gameThread;
 	
+	
+	//Player position
+	int playerX = 100;
+	int playerY = 100;
+	int playerSpeed = 5;
 	
 	
 	public Game_Window() {
 		this.setPreferredSize(new Dimension(max_Screen_Width, max_Screen_Height));
 		this.setBackground(Color.black);
 		this.setDoubleBuffered(true);
+		this.addKeyListener(keyHandler);
+		this.setFocusable(true);
 	}
 
 	public void Start_Game_Thread() {
@@ -35,14 +46,55 @@ public class Game_Window extends JPanel implements Runnable{
 	}
 
 	@Override
-	public void run() {
+	public void run() {		
+		
+		double drawInterwal = 1000000000/FPS; //0,016 seconds
+		double nextDrawTime = System.nanoTime() + drawInterwal;
+		
 		while (gameThread != null) {
+			
+			
+			update();
+			
+			repaint();
+			
+			
+			try {
+				double remainingTime  = nextDrawTime - System.nanoTime();
+				remainingTime = remainingTime/1000000; //Convert to miliseconds
+				
+				if(remainingTime < 0) {
+					remainingTime = 0;
+				}
+				Thread.sleep((long) remainingTime);
+				
+				nextDrawTime += drawInterwal;
+				
+			} catch (InterruptedException e) {
+				
+				
+				e.printStackTrace();
+			}
 			
 		}
 	}
 	
 	public void update() {
+		if(keyHandler.upPressed == true) {
+			playerY -= playerSpeed;
+		}
 		
+		if(keyHandler.downPressed == true) {
+			playerY += playerSpeed;
+		}
+		
+		if(keyHandler.leftPressed == true) {
+			playerX -= playerSpeed;
+		}
+		
+		if(keyHandler.rightPressed == true) {
+			playerX += playerSpeed;
+		}
 	}
 
 	public void paintComponent(Graphics g) {
@@ -53,7 +105,7 @@ public class Game_Window extends JPanel implements Runnable{
 		
 		g2.setColor(Color.white);
 		
-		g2.fillRect(100, 100, tileSize, tileSize);
+		g2.fillRect(playerX, playerY, tileSize, tileSize);
 		
 		g2.dispose();
 		
